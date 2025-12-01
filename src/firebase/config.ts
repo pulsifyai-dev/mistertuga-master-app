ae2e6af024eebeab3a39fbcb64dcefb41bf14604
Bruno Silva <bruno@pulsifyai.com>
2025-12-01 20:07:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9297560854-48d83",
+  "appId": "1:358058888580:web:e9826046d9848c84a8e1d1",
+  "apiKey": "AIzaSyDTKmDyiIgauwj7ZX_9l4gtjHD60z1Dyg8",
+  "authDomain": "studio-9297560854-48d83.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "358058888580"
+};
