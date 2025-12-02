a2f697a45456a2541c644ff1eeeac9e06cbb753a
Bruno Silva <bruno@pulsifyai.com>
2025-12-02 00:21:56 +0000

    Try fixing this error: `Console Error: Invalid DOM property `stroke-widt

--- src/app/(app)/master-shopify-orders/page.tsx
+++ src/app/(app)/master-shopify-orders/page.tsx
@@ -52,7 +52,7 @@ const editOrderSchema = z.object({
 type EditOrderSchema = z.infer<typeof editOrderSchema>;
 
 // SVG Flag Components
-const FlagPT = () => <svg xmlns="http://www.w3.org/2000/svg" width="20" height="15" viewBox="0 0 20 15"><path fill="#006233" d="M0 0h8v15H0z"/><path fill="#D21034" d="M8 0h12v15H8z"/><circle cx="8" cy="7.5" r="2.5" fill="#FFE000"/><path fill="none" stroke="#D21034" stroke-width="0.5" d="M8 5a2.5 2.5 0 000 5m-1.5-3.5h3"/></svg>;
+const FlagPT = () => <svg xmlns="http://www.w3.org/2000/svg" width="20" height="15" viewBox="0 0 20 15"><path fill="#006233" d="M0 0h8v15H0z"/><path fill="#D21034" d="M8 0h12v15H8z"/><circle cx="8" cy="7.5" r="2.5" fill="#FFE000"/><path fill="none" stroke="#D21034" strokeWidth="0.5" d="M8 5a2.5 2.5 0 000 5m-1.5-3.5h3"/></svg>;
 const FlagDE = () => <svg xmlns="http://www.w3.org/2000/svg" width="20" height="15" viewBox="0 0 20 15"><path fill="#000" d="M0 0h20v5H0z"/><path fill="#D00" d="M0 5h20v5H0z"/><path fill="#FFCE00" d="M0 10h20v5H0z"/></svg>;
 const FlagES = () => <svg xmlns="http://www.w3.org/2000/svg" width="20" height="15" viewBox="0 0 20 15"><path fill="#C60B1E" d="M0 0h20v3.75H0zM0 11.25h20V15H0z"/><path fill="#FFC400" d="M0 3.75h20v7.5H0z"/></svg>;
 
