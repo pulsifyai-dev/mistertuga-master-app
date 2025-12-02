3e074d0ba108c03184a3db19e88510b26bf50bbf
Bruno Silva <bruno@pulsifyai.com>
2025-12-02 01:24:03 +0000

    por exmeplo este link nao está a parecer: https://cdn.shopify.com/s/file

--- next.config.ts
+++ next.config.ts
@@ -14,26 +14,18 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'cdn.shopify.com',
-        port: '',
-        pathname: '/**',
       }
     ],
   },
